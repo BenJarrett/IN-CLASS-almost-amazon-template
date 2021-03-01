97da68e07e19ee00518920b3093c8df27a9b74d0
Ben Jarrett <b.cl.jarrett@gmail.com>
2021-03-01 14:35:43 -0600

    caught up

--- src/javascripts/views/startApp.js
+++ src/javascripts/views/startApp.js
@@ -1,8 +1,10 @@
+import { emptyBooks, showBooks } from '../components/books';
 import logoutButton from '../components/buttons/logoutButton';
 import domBuilder from '../components/domBuilder';
 import navBar from '../components/navBar';
 import domEvents from '../events/domEvents';
 import navigationEvents from '../events/navigationEvents';
+import { getBooks } from '../helpers/data/bookData';
 
 const startApp = () => {
   domBuilder(); // BUILD THE DOM
@@ -10,6 +12,14 @@ const startApp = () => {
   navBar(); // DYNAMICALLY ADD THE NAV
   logoutButton(); // ADD THE LOGOUT BUTTON COMPONENT
   navigationEvents(); // ATTACH THE EVENT LISTENERS TO THE NAVBAR
+  // Put all books on the DOM
+  getBooks().then((booksArray) => {
+    if (booksArray.length) {
+      showBooks(booksArray);
+    } else {
+      emptyBooks();
+    }
+  });
 };
 
 export default startApp;
